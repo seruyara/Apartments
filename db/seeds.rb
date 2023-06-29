43876ee5007fcc1fd29660e0169e4259494767d1
Sheila Seruya <Sheilaseruya@gmail.com>
2023-06-29 11:50:28 +0300

    seed file is working

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+puts 'seeding apartment 🏠..'
+# Generate fake data for Apartment model
+10.times do
+  Apartment.create(number: Faker::Number.number(digits: 3))
+end
+
+# Generate fake data for Tenant model
+10.times do
+  Tenant.create(name: Faker::Name.name, age: Faker::Number.between(from: 18, to: 65))
+end
+
+# Generate fake data for Lease model
+10.times do
+  Lease.create(rent: Faker::Number.decimal(l_digits: 4, r_digits: 2))
+end
+
+puts 'done seeding ...'
