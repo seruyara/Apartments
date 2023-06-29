5a18536ee0a29d253d87ba52fadad6f1fc50784e
BRIAN BETT <+>
2023-06-29 14:05:57 +0300

    adding routes-brian

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
-Rails.application.routes.draw do
-  resources :leases
-  resources :tenants
-  resources :apartments
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+Rails.application.routes.draw do 
+
+  resources :apartments 
+    
+
+  
+   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  get '/apartments/:apartment_id/tenants', to: 'tenants#index_by_apartment'
+  get '/apartment/:apartment_id/leases', to: 'leases#index_by_apartment'
+  
+
 end
